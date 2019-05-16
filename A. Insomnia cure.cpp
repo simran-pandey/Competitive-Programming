4893f2b4bb4e4f01356e451db2ac791cc5ae57e1
Simran Pandey <pandeysimran97@gmail.com>
2019-05-17 00:56:28 +0530

    Create A. Insomnia cure.cpp

---
+++ A. Insomnia cure.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{ 
+    int k,l,m,n,d,count;
+    
+    cin>>k>>l>>m>>n>>d;
+    
+    count = d;
+    
+    if(k==1||l==1||m==1||n==1) //corner case
+    {
+        cout<<d<<endl;
+    }
+    
+    else
+    {
+        for(int i=1;i<=d;i++)
+        {
+            if(i%k!=0 && i%l!=0 && i%m!=0 && i%n!=0)
+            {
+                count--;
+            }
+        }
+    
+        cout<<count<<endl;
+    }
+    
+return 0;
+    
+}
