b1763ce64c6c87e6ea37a2c3176b2141a58cd35b
Simran Pandey <pandeysimran97@gmail.com>
2019-05-22 20:56:14 +0530

    Create SPOJ - Life, the Universe, and Everything.cpp

---
+++ SPOJ - Life, the Universe, and Everything.cpp
@@ -0,0 +1,18 @@
+# 23659086		2019-04-19 18:25:57	Life, the Universe, and Everything	accepted 0.00	16M	CPP
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	
+int a;
+ 
+do{
+	
+	cin>>a;
+	if(a!=42&&a<100&&a>(-100))
+	  {
+	  	cout<<a<<"\n";
+	  }
+  }while(a!=42&&a<100&&a>(-100));
+} 
